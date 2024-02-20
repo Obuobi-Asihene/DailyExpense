c2c7081ddcef5b6ebadaf7bab3fbe844309f1330
AzureYakubu <obsmediaofficial@gmail.com>
2024-02-20 23:02:31 +0000

    updated migration

--- DailyExpenseAPI/Migrations/20240220225640_UpdatedMigration.Designer.cs
+++ DailyExpenseAPI/Migrations/20240220225640_UpdatedMigration.Designer.cs
@@ -4,6 +4,7 @@ using DailyExpenseAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DailyExpenseAPI.Migrations
 {
     [DbContext(typeof(DailyExpenseDbContext))]
-    partial class DailyExpenseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240220225640_UpdatedMigration")]
+    partial class UpdatedMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DailyExpenseAPI/Migrations/20240220225640_UpdatedMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyExpenseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyExpenses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalExpense = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyExpenses", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyExpenses");
+        }
+    }
+}
