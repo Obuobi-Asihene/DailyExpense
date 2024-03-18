f3babb053459c1a113f61ad46a7cff9aa2691863
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-18 08:35:35 +0000

    added MAUI project

---
+++ DailyExpenseUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyExpenseUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
